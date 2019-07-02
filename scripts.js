46cb7860979ba7b3c14016031828bdcf33c17b1c
Student <example@example.com>
2019-07-01 19:53:34 -0700

    3rd commit

--- scripts.js
+++ scripts.js
@@ -5,14 +5,36 @@ function AddressBook () {
 }
 
 AddressBook.prototype.addContact = function(contact) {
-  contact.id= this.assignID();
+  contact.id= this.assignId();
   this.contacts.push(contact);
 }
 
-AddressBook.prototype.assignID= function(){
+AddressBook.prototype.assignID= function() {
   this.currentId += 1;
-  return this.currentID
+  return this.currentId;
 }
+
+AddressBook.prototype.findContact= function(id); {
+  for (var i=0; i<this.contacts.length;i++) {
+    if (this.contacts[i] .id==id) {
+      return this.contacts[i];
+    }
+  }
+};
+return false;
+}
+
+AddressBook.prototype.deleteContact= function(id) {
+  for (var i=0; i< this.contacts.lenth; i++) {
+    if (this.contacts [i].id ==id) {
+      delete this.contacts[i];
+      return true;
+      }
+    }
+  };
+  return false;
+}
+
 
 //Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
